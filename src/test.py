eb75419c02c7f64a9f5bd95bda76b492fdd77699
Ivy Wang <s1768094@ed.ac.uk>
2019-11-16 02:34:06 +0000

    solve for angle

--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sci
+from scipy.optimize import fsolve, leastsq
 
 def transform(theta1,theta2,theta3,theta4):
         # T10 = transform_matrix(0, 0, 0, theta1)
@@ -20,10 +22,21 @@ def transform_matrix(alpha, r, d, theta):
         [0,np.sin(alpha), np.cos(alpha), d],
         [0,0,0,1]
     ])
+def F(x, data):
+    t1=x[0]
+    t2=x[1]
+    t3=x[2]
+    return ((-3*np.cos(t1)*np.sin(t2)*np.cos(t3)+3*np.sin(t1)*np.sin(t3)-data[0]),
+            (-3*np.sin(t1)*np.sin(t2)*np.cos(t3)-3*np.cos(t1)*np.sin(t3)-data[1]),
+            (3*np.cos(t2)*np.cos(t3)+2-data[2]))
 
 
 X = np.array([0,0,0,1])
 # print(T)
-T=np.round(transform(0,0,np.pi/4,0), 3)
+
+T=np.round(transform(0,0,np.pi/3,0), 3)
 print(T.dot(np.array([0,0,0,1])))
 # print(np.linalg.inv(T))
+angles = [0,0,0]
+r = leastsq(F, angles, args=[0,-2.598, 3.5])
+print(r)
